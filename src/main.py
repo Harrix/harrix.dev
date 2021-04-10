cedeb15a9c5d836d5cddf38a6bd79107b4d4a92b
Sergienko Anton <sergienkoanton@mail.ru>
2021-04-10 14:24:38 +0300

    Add StaticSiteGenerator class

--- src/main.py
+++ src/main.py
@@ -2,13 +2,9 @@ import harrixpyssg as hsg
 
 
 def main():
-    # markdown_filename = 'C:/Harrix/GitHub/harrix.dev-blog-2019/2019-07-23-add-2-num-android/2019-07-23-add-2-num-android.md'
-    # output_path = '../dist'
-    # hsg.MarkdownToHtml(markdown_filename, output_path).start()
-
-    markdown_path = 'C:/Harrix/GitHub/harrix.dev-blog-2017/'
+    markdown_paths = ['C:\Harrix\GitHub\harrix.dev\src\_posts']
     output_path = './dist'
-    hsg.Dir_md_to_dir_html(markdown_path, output_path).start()
+    hsg.StaticSiteGenerator(markdown_paths, output_path).start()
 
 
 if __name__ == '__main__':
